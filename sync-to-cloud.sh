83bc39dd992f41fe2ba181135519e2ae9b6f33e7
joshegladwell <joshegladwell@gmail.com>
2022-12-05 21:18:02 -0500

    Edit

--- sync-to-cloud.sh
+++ sync-to-cloud.sh
@@ -2,8 +2,8 @@
 rsync -alvr --delete 501-project-website gladwell@gladwell.georgetown.domains:/home/gladwell/public_html/
 
 # PUSH GIT REPO TO THE CLOUD FOR BACKUP
-DATE=$(date +"DATE-%Y-%m-%d-TIME-%H-%M-%S")
-message="GITHUB-UPLOAD:$DATE";
+echo Commit message: ;
+read message;
 echo "commit message = "$message; 
 git add ./; 
 git commit -m $message; 
